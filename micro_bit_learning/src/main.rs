316536392314de6ee62c4b4d149147b21cb8cc36
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-10-08 12:34:57 +0200

    blinking led

--- micro_bit_learning/src/main.rs
+++ micro_bit_learning/src/main.rs
@@ -1,18 +1,29 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use cortex_m::asm::nop;
+use embedded_hal::digital::{OutputPin, PinState};
+use hal::pac;
+use hal::gpio::Level;
+use nrf52833_hal as hal;
 use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
+
 
 #[entry]
 fn main() -> ! {
-   let mut x:usize = 0;
+    let mut is_on = false;
+    let p = pac::Peripherals::take().unwrap();
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+    let _col1 = port0.p0_28.into_push_pull_output(Level::Low);
+    let mut row1 = port0.p0_21.into_push_pull_output(Level::Low);
     loop {
-        x+=1;
-        for _ in 0..x{
+        let _ = row1.set_state(PinState::from(is_on));
+        for _ in 0..200_0000 {
             nop()
-        }
+        }  
+            
+        is_on = !is_on;
+        
     }
 }
